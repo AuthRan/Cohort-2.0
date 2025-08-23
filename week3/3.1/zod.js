9085a999bcacc81f60ad52ba43972ac2b3777333
Ashutosh Ranjan <authran.off@gmail.com>
2025-08-24 02:13:20 +0530

    .

--- week3/3.1/zod.js
+++ week3/3.1/zod.js
@@ -1,9 +1,15 @@
 const zod = require('zod')
 
 const validate = (arr) =>{
-    const schema = zod.array(zod.number())
+    const schema = zod.object({
+        email : zod.string().email(),
+        password : zod.string().min(8)
+    })
     const reponse = schema.safeParse(arr)
     console.log(reponse)
 }
 
-validate([1,"t",4])
+validate({
+    email : "test@gmail.com",
+    password : "1234564846541"
+})
