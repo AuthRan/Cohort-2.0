442bbd6ee499d8dae6b5c9b56ac10dc679648614
Ashutosh Ranjan <authran.off@gmail.com>
2026-01-03 00:51:59 +0530

    Implementing validation using Zod and index.js

--- week5/backend/index.js
+++ week5/backend/index.js
@@ -1,16 +1,37 @@
 const express = require('express');
+const { createTodo, updateTodo } = require('./types');
 const app = express();
 
 app.use(express.json())
 
+// Post is used to put something onto db
 app.post('/todo', (req, res) => {
-
+    const createPayload = req.body
+    const parsedPayload = createTodo.safeParse(createPayload)
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg : "You sent the wrong inputs"
+        })
+        return ;
+    }
+    // Put it in mongoDB
 })
-app.get('/todos', (req, res) => {
-    
-});
-app.put('/completed', (req, res) => {
 
+app.get('/todos', (req, res) => {
+
+});
+
+// Put is used to update things on the db
+app.put('/completed', (req, res) => {
+    const updatePayload = req.body
+    const parsedPayload = updateTodo.safeParse(updatePayload)
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg : "You send the wrong inputs"
+        })
+        return ;
+    }
+    
 })
 
 app.listen(3000, () => {
