9eadcdb0e7ccee01bfc49cbb8f0c14f9c134fd7b
Ashutosh Ranjan <authran.off@gmail.com>
2026-01-03 01:19:37 +0530

    Completed backend :)

--- week5/backend/index.js
+++ week5/backend/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { createTodo, updateTodo } = require('./types');
+const { todo } = require('./db');
 const app = express();
 
 app.use(express.json())
 
 // Post is used to put something onto db
-app.post('/todo', (req, res) => {
+app.post('/todo', async (req, res) => {
     const createPayload = req.body
     const parsedPayload = createTodo.safeParse(createPayload)
     if (!parsedPayload.success) {
@@ -16,14 +17,25 @@ app.post('/todo', (req, res) => {
         return ;
     }
     // Put it in mongoDB
+    await todo.create({
+        title : createPayload.title,
+        description : createPayload.description,
+        completed : false
+    })
+    res.json({
+        msg : "Todo Created"
+    })
 })
 
-app.get('/todos', (req, res) => {
-
+app.get('/todos', async (req, res) => {
+    const todos = await todo.find({})  // Gives back everything
+    res.json({
+        todos
+    })
 });
 
 // Put is used to update things on the db
-app.put('/completed', (req, res) => {
+app.put('/completed', async (req, res) => {
     const updatePayload = req.body
     const parsedPayload = updateTodo.safeParse(updatePayload)
     if (!parsedPayload.success) {
@@ -32,6 +44,16 @@ app.put('/completed', (req, res) => {
         })
         return ;
     }
+
+    // .update({what to update usually by _id}, {what to update})
+    await todo.update({
+        _id : req.body.id
+    }, {
+        completed : true
+    })
+    res.json({
+        msg : "Todo marked as completed"
+    })
 
 })
 
